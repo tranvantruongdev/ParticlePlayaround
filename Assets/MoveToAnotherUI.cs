85ae1279edc8f74cabb7122a1fe4009c6fa0a403
Zed Darkness <ngo2016@gmail.com>
2020-12-05 07:48:17 +0700

    coin to meter particle

---
+++ Assets/MoveToAnotherUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToAnotherUI : MonoBehaviour
+{
+    //Declare RectTransform in script
+    RectTransform faceButton;
+    //The new position of your button
+    [SerializeField]
+    RectTransform newPos;
+    [SerializeField]
+    float speed = 15f;
+    public ParticleSystem ParticleSystem;
+
+    void Start()
+    {
+        //Get the RectTransform component
+        faceButton = GetComponent<RectTransform>();
+        //Debug.Log(faceButton.position.ToString());
+        //newPos.position -= new Vector3(0f, 5f, 0f);
+        //Debug.Log(newPos.position.ToString());
+    }
+
+    void LateUpdate()
+    {
+        if (faceButton.position == newPos.position)
+        {
+            ParticleSystem.Clear();
+            return;
+        }
+        //Update the localPosition towards the newPos
+        faceButton.position = Vector3.MoveTowards(faceButton.position, newPos.position,
+            speed * Time.deltaTime);
+    }
+
+}
--- Assets/ParticleFollow.cs
+++ Assets/ParticleFollow.cs
@@ -8,32 +8,32 @@ public class ParticleFollow : MonoBehaviour
     public Transform target;
 	public ParticleSystem m_System;
 	ParticleSystem.Particle[] m_Particles;
-	int index;
 
 	[Tooltip("Cap the maximum speed to prevent particle from being flung too far from the missed target.")]
 	public float maxSpeed = 50f;
 
 	private void LateUpdate()
     {
-        Vector3 targetPos = cam.ScreenToWorldPoint(new Vector3(target.position.x, 
-            target.position.y, target.position.z * -1));
-
+			//set target's position based on camera
+			//Vector3 targetPos = cam.ScreenToWorldPoint(new Vector3(target.position.x,
+			//	target.position.y, target.position.z * -1));
+		Vector3 targetPos = cam.ScreenToWorldPoint(new Vector3(target.position.x,
+				target.position.y, 0));
 		int numParticlesAlive = m_System.GetParticles(m_Particles);
-		for (int i = 0; i < numParticlesAlive; i++)
-		{
-			float ted = (target.position - this.transform.position).sqrMagnitude + 0.001f;
-			Vector3 diff = target.position - m_Particles[i].position;
-			float diffsqrm = diff.sqrMagnitude;
-			float face = Vector3.Dot(m_Particles[i].velocity.normalized, diff.normalized);
-			float f = Mathf.Abs((ted - diffsqrm) / ted) * ted * (face + 1.001f);
-			//m_Particles[i].velocity = Vector3.ClampMagnitude(m_Particles[i].velocity + diff * speed * 0.01f * f, maxSpeed);
-			float t = 0;
-			t += Time.deltaTime;
-			m_Particles[i].velocity = Vector3.ClampMagnitude(
-				Vector3.Slerp(m_Particles[i].velocity, m_Particles[i].velocity + diff * 0.01f * f, t), 
-				maxSpeed);			
-		}
-		m_System.SetParticles(m_Particles, numParticlesAlive);
+			for (int i = 0; i < numParticlesAlive; i++)
+			{
+				float ted = (targetPos - this.transform.position).sqrMagnitude + 0.001f;
+				Vector3 diff = targetPos - m_Particles[i].position;
+				float diffsqrm = diff.sqrMagnitude;
+				float face = Vector3.Dot(m_Particles[i].velocity.normalized, diff.normalized);
+				float f = Mathf.Abs((ted - diffsqrm) / ted) * ted * (face + 1.001f);
+				float t = 0;
+				t += Time.deltaTime;
+				m_Particles[i].velocity = Vector3.ClampMagnitude(
+					Vector3.Slerp(m_Particles[i].velocity, m_Particles[i].velocity + diff * 0.01f * f, t),
+					maxSpeed);
+			}
+			m_System.SetParticles(m_Particles, numParticlesAlive);
 	}
 
     // Start is called before the first frame update
