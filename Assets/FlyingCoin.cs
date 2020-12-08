9ca0fa7621d2a412c6fd57504687dc81fa4cf3b8
Zed Darkness <ngo2016@gmail.com>
2020-12-08 16:36:46 +0700

    day 11 - finally particle fly to desired position

--- Assets/FlyingCoin.cs
+++ Assets/FlyingCoin.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class FlyingCoin : MonoBehaviour
 {
-    [SerializeField]
-    private ParticleSystem particleSystem;
+	public Transform Target;
 
-    private void OnEnable()
+	private ParticleSystem system;
+
+	private static ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1000];
+	int count;
+
+	void Start()
+	{
+		if (system == null)
+			system = GetComponent<ParticleSystem>();
+
+		if (system == null)
+		{
+			enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		count = system.GetParticles(particles);
+
+		for (int i = 0; i < count; i++)
+		{
+			ParticleSystem.Particle particle = particles[i];
+
+			Vector3 v1 = system.transform.TransformPoint(particle.position);
+			Vector3 v2 = Target.transform.position;
+
+			Vector3 tarPosi = (v2 - v1) * (particle.remainingLifetime / particle.startLifetime);
+
+			Debug.Log(tarPosi.ToString());
+
+            if (tarPosi.y < 1f)
+            {
+				system.Clear();
+				system.Stop(system);
+				return;
+            }
+			particle.position = system.transform.InverseTransformPoint(v2 - tarPosi);
+			particles[i] = particle;
+		}
+
+		system.SetParticles(particles, count);
+	}
+
+	private void OnEnable()
     {
-        particleSystem.Play();
+        //particleSystem.Play();
     }
 
     private void OnDisable()
     {
-        particleSystem.Clear();
+        //particleSystem.Clear();
     }
 }
