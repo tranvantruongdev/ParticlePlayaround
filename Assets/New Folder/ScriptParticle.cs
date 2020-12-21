69b40656f47c56399b8bc86926b542bf2683ccf9
Zed Darkness <ngo2016@gmail.com>
2020-12-21 17:26:45 +0700

    clean half of ui particle after amount of time

--- Assets/New Folder/ScriptParticle.cs
+++ Assets/New Folder/ScriptParticle.cs
@@ -10,52 +10,52 @@ public struct ParticleStruct
     public Transform StartPos;
     public Transform EndPos;
     public GameObject ParObj;
-    public ParticleSystem ParSys;
-    public ParticleSystem PressedParSys;
     public bool RunBool;
     [Range(0.1f, 5.0f)]
     public float time;
     [HideInInspector]
     public float speed;
-    public Queue<GameObject> UiParticlePool { get; set; }
-    public Queue<GameObject> UiPressedParticlePool { get; set; }
     public float CleaningInterval;
 }
 
 public class ScriptParticle : MonoBehaviour
 {
-    //public ParticleStruct ParStruct = new ParticleStruct();
     public ParticleStruct[] ParStruct2;
 
     public GameObject ParticlePrefab;
+    public GameObject ParticleIdlePrefab;
+
     public GameObject CanvasPar;
+
     List<ParticleStruct> ListObjPar = new List<ParticleStruct>();
     List<ParticleStruct> ListObjAvail = new List<ParticleStruct>();
 
+    List<ParticleStruct> ListIdleObjPar = new List<ParticleStruct>();
+    List<ParticleStruct> ListIdleObjAvail = new List<ParticleStruct>();
+
     private void Start()
     {
-        //ParStruct.Btn.onClick.AddListener(() => Click());
         foreach (var item in ParStruct2)
         {
             item.Btn.onClick.AddListener(() =>
             {
                 Click(item);
             });
-
         }
+        InvokeRepeating(nameof(CleanUiParticleOvertime), ParStruct2[0].CleaningInterval,
+            ParStruct2[0].CleaningInterval);
     }
 
     private void Update()
     {
         for (int i = 0; i < ListObjPar.Count; i++)
         {
-            if (ListObjPar[i].RunBool)
+            if (ListObjPar[i].RunBool && ListObjPar[i].ParObj != null)
             {
                 ListObjPar[i].ParObj.transform.position = Vector3.MoveTowards(
                     ListObjPar[i].ParObj.transform.position,
                     ListObjPar[i].EndPos.position,
-                    ListObjPar[i].time * Time.deltaTime * 10
-                    );
+                    ListObjPar[i].time * Time.deltaTime * 10);
 
                 if (ListObjPar[i].ParObj.transform.position == ListObjPar[i].EndPos.position)
                 {
@@ -73,10 +73,28 @@ public class ScriptParticle : MonoBehaviour
 
     public void Click(ParticleStruct item)
     {
+        if (ListIdleObjAvail.Count == 0)
+        {
+            GameObject go2 = Instantiate(ParticleIdlePrefab, CanvasPar.transform, false);
+            go2.transform.position = item.StartPos.position;
+            go2.SetActive(true);
+            item.ParObj = go2;
+            ListIdleObjPar.Add(item);
+        }
+        else
+        {
+            ParticleStruct itemAvail2 = ListIdleObjAvail[0];
+            itemAvail2.ParObj.SetActive(true);
+
+            itemAvail2.ParObj.transform.position = item.StartPos.position;
+
+            ListObjPar.Add(itemAvail2);
+            ListObjAvail.Remove(ListObjAvail[0]);
+        }
+
         if (ListObjAvail.Count == 0)
         {
             GameObject go = Instantiate(ParticlePrefab, CanvasPar.transform, false);
-            //set pos, chay run 
             go.transform.position = item.StartPos.position;
             item.ParObj = go;
             item.RunBool = true;
@@ -99,73 +117,18 @@ public class ScriptParticle : MonoBehaviour
         }
     }
 
-    private void AddUiParticle(int index, Queue<GameObject> pool, int amout)
-    {
-        for (int i = 0; i < amout; i++)
-        {
-            GameObject particle;
-            particle = Instantiate(ParStruct2[index].ParSys.gameObject, transform);
-            particle.SetActive(false);
-            pool.Enqueue(particle);
-        }
-    }
-
-    private void AddUiPressParticle(int index, Queue<GameObject> pool, int amout)
-    {
-        for (int i = 0; i < amout; i++)
-        {
-            GameObject particle;
-            particle = Instantiate(ParStruct2[index].PressedParSys.gameObject, transform);
-            particle.SetActive(false);
-            pool.Enqueue(particle);
-        }
-    }
-
     private void CleanUiParticleOvertime()
     {
-        for (int j = 0; j < ParStruct2.Length; j++)
+        if (ListObjAvail.Count > 1)
         {
-            GameObject uiParticle;
-            int loopTimes = ParStruct2[j].UiParticlePool.Count;
-            for (int i = 1; i < loopTimes; i++)
+            for (int i = 1; i < ListObjAvail.Count; i++)
             {
-                uiParticle = ParStruct2[j].UiParticlePool.Dequeue();
-                if (!uiParticle.activeInHierarchy)
+                if (ListObjAvail[i].ParObj != null && ListObjAvail[i].ParObj.activeSelf == false && ListObjAvail[i].RunBool == false)
                 {
-                    Destroy(uiParticle);
+                    Destroy(ListObjAvail[i].ParObj.gameObject);
+                    ListObjAvail.Remove(ListObjAvail[i]);
                 }
             }
-
-            int loopTimes2 = ParStruct2[j].UiPressedParticlePool.Count;
-            for (int i = 1; i < loopTimes2; i++)
-            {
-                uiParticle = ParStruct2[j].UiPressedParticlePool.Dequeue();
-                if (!uiParticle.activeInHierarchy)
-                {
-                    Destroy(uiParticle);
-                }
-            }
-        }
-    }
-
-    IEnumerator PutBackToPool(GameObject gameObjectToPool, Queue<GameObject> pool, float time = 1.0f)
-    {
-        yield return new WaitForSeconds(time);
-        gameObjectToPool.gameObject.SetActive(false);
-        pool.Enqueue(gameObjectToPool);
-    }
-
-    IEnumerator FlyTo(GameObject objectToMove, Transform end, float speed)
-    {
-        while (objectToMove.transform.position != end.position)
-        {
-            objectToMove.transform.position = Vector3.MoveTowards(
-                objectToMove.transform.position,
-                end.position,
-                speed * Time.deltaTime);
-
-            // Wait a frame and move again.
-            yield return null;
         }
     }
 }
