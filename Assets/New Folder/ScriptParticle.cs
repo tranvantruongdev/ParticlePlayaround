db5f43d914109037de1272e05a75a0cc39e5e5fc
Zed Darkness <ngo2016@gmail.com>
2020-12-21 11:41:55 +0700

    day 22 - uiparticle vanila

--- Assets/New Folder/ScriptParticle.cs
+++ Assets/New Folder/ScriptParticle.cs
@@ -9,111 +9,163 @@ public struct ParticleStruct
     public Button Btn;
     public Transform StartPos;
     public Transform EndPos;
+    public GameObject ParObj;
     public ParticleSystem ParSys;
+    public ParticleSystem PressedParSys;
+    public bool RunBool;
     [Range(0.1f, 5.0f)]
     public float time;
     [HideInInspector]
     public float speed;
     public Queue<GameObject> UiParticlePool { get; set; }
     public Queue<GameObject> UiPressedParticlePool { get; set; }
+    public float CleaningInterval;
 }
 
 public class ScriptParticle : MonoBehaviour
 {
     //public ParticleStruct ParStruct = new ParticleStruct();
     public ParticleStruct[] ParStruct2;
-    bool run = false;
-    int currentIndex = 0;
+
+    public GameObject ParticlePrefab;
+    public GameObject CanvasPar;
+    List<ParticleStruct> ListObjPar = new List<ParticleStruct>();
+    List<ParticleStruct> ListObjAvail = new List<ParticleStruct>();
 
     private void Start()
     {
         //ParStruct.Btn.onClick.AddListener(() => Click());
-        for (int i = 0; i < ParStruct2.Length; i++)
+        foreach (var item in ParStruct2)
         {
-            int x = i;
-            ParStruct2[i].Btn.onClick.AddListener(() => Click(x));
+            item.Btn.onClick.AddListener(() =>
+            {
+                Click(item);
+            });
+
         }
-
-        //allocate some pool
-        ParStruct2[0].UiParticlePool = new Queue<GameObject>();
-        ParStruct2[0].UiPressedParticlePool = new Queue<GameObject>();
-    }
-
-    private void AddUiParticle(int amout)
-    {
-        for (int i = 0; i < amout; i++)
-        {
-            GameObject particle;
-            particle = Instantiate(ParStruct2[0].ParSys.gameObject, transform);
-            particle.SetActive(false);
-            ParStruct2[0].UiParticlePool.Enqueue(particle);
-        }
-    }
-
-    public void Click(int index)
-    {
-        currentIndex = index;
-        if (ParStruct2[index].ParSys.isPlaying)
-        {
-            ParStruct2[index].ParSys.Clear();
-            ParStruct2[index].ParSys.Stop();
-        }
-
-        ParStruct2[index].ParSys.transform.position = ParStruct2[index].StartPos.position;
-        var particleMain = ParStruct2[index].ParSys.main;
-        particleMain.startLifetime = ParStruct2[index].time;
-        ParStruct2[index].speed = Vector3.Distance(ParStruct2[index].StartPos.position,
-            ParStruct2[index].EndPos.position) / (ParStruct2[index].time + Time.deltaTime);
-        ParStruct2[index].ParSys.Play();
-        run = true;
     }
 
     private void Update()
     {
-        if (ParStruct2[currentIndex].ParSys.transform.position == ParStruct2[currentIndex].EndPos.position)
+        for (int i = 0; i < ListObjPar.Count; i++)
         {
-            run = false;
-        }
+            if (ListObjPar[i].RunBool)
+            {
+                ListObjPar[i].ParObj.transform.position = Vector3.MoveTowards(
+                    ListObjPar[i].ParObj.transform.position,
+                    ListObjPar[i].EndPos.position,
+                    ListObjPar[i].time * Time.deltaTime * 10
+                    );
 
-        if (run == true)
-        {
-            ParStruct2[currentIndex].ParSys.transform.position =
-                Vector3.MoveTowards(ParStruct2[currentIndex].ParSys.transform.position,
-                ParStruct2[currentIndex].EndPos.position, ParStruct2[currentIndex].speed * Time.deltaTime);
+                if (ListObjPar[i].ParObj.transform.position == ListObjPar[i].EndPos.position)
+                {
+                    ParticleStruct item = ListObjPar[i];
+                    item.RunBool = false;
+                    item.ParObj.SetActive(false);
+                    ListObjPar[i] = item;
+
+                    ListObjAvail.Add(item);
+                    ListObjPar.Remove(ListObjPar[i]);
+                }
+            }
         }
     }
 
-    #region Single Version
-    //public void Click()
-    //{
-    //    if (ParStruct.ParSys.isPlaying)
-    //    {
-    //        ParStruct.ParSys.Clear();
-    //        ParStruct.ParSys.Stop();
-    //    }
+    public void Click(ParticleStruct item)
+    {
+        if (ListObjAvail.Count == 0)
+        {
+            GameObject go = Instantiate(ParticlePrefab, CanvasPar.transform, false);
+            //set pos, chay run 
+            go.transform.position = item.StartPos.position;
+            item.ParObj = go;
+            item.RunBool = true;
+            ListObjPar.Add(item);
+        }
+        else
+        {
+            ParticleStruct itemAvail = ListObjAvail[0];
 
-    //    ParStruct.ParSys.transform.position = ParStruct.StartPos.position;
-    //    var particleMain = ParStruct.ParSys.main;
-    //    particleMain.startLifetime = ParStruct.time;
-    //    ParStruct.speed = Vector3.Distance(ParStruct.StartPos.position,
-    //        ParStruct.EndPos.position) / (ParStruct.time + Time.deltaTime);
-    //    ParStruct.ParSys.Play();
-    //    run = true;
-    //}
+            itemAvail.ParObj.SetActive(true);
+            itemAvail.StartPos = item.StartPos;
+            itemAvail.EndPos = item.EndPos;
+            itemAvail.time = item.time;
 
-    //private void Update()
-    //{
-    //    if (ParStruct.ParSys.transform.position == ParStruct.EndPos.position)
-    //    {
-    //        run = false;
-    //    }
+            itemAvail.ParObj.transform.position = item.StartPos.position;
+            itemAvail.RunBool = true;
 
-    //    if (run == true)
-    //    {
-    //        ParStruct.ParSys.transform.position =
-    //            Vector3.MoveTowards(ParStruct.ParSys.transform.position,
-    //            ParStruct.EndPos.position, ParStruct.speed * Time.deltaTime);
-    //    }
-    //}
-    #endregion
+            ListObjPar.Add(itemAvail);
+            ListObjAvail.Remove(ListObjAvail[0]);
+        }
+    }
+
+    private void AddUiParticle(int index, Queue<GameObject> pool, int amout)
+    {
+        for (int i = 0; i < amout; i++)
+        {
+            GameObject particle;
+            particle = Instantiate(ParStruct2[index].ParSys.gameObject, transform);
+            particle.SetActive(false);
+            pool.Enqueue(particle);
+        }
+    }
+
+    private void AddUiPressParticle(int index, Queue<GameObject> pool, int amout)
+    {
+        for (int i = 0; i < amout; i++)
+        {
+            GameObject particle;
+            particle = Instantiate(ParStruct2[index].PressedParSys.gameObject, transform);
+            particle.SetActive(false);
+            pool.Enqueue(particle);
+        }
+    }
+
+    private void CleanUiParticleOvertime()
+    {
+        for (int j = 0; j < ParStruct2.Length; j++)
+        {
+            GameObject uiParticle;
+            int loopTimes = ParStruct2[j].UiParticlePool.Count;
+            for (int i = 1; i < loopTimes; i++)
+            {
+                uiParticle = ParStruct2[j].UiParticlePool.Dequeue();
+                if (!uiParticle.activeInHierarchy)
+                {
+                    Destroy(uiParticle);
+                }
+            }
+
+            int loopTimes2 = ParStruct2[j].UiPressedParticlePool.Count;
+            for (int i = 1; i < loopTimes2; i++)
+            {
+                uiParticle = ParStruct2[j].UiPressedParticlePool.Dequeue();
+                if (!uiParticle.activeInHierarchy)
+                {
+                    Destroy(uiParticle);
+                }
+            }
+        }
+    }
+
+    IEnumerator PutBackToPool(GameObject gameObjectToPool, Queue<GameObject> pool, float time = 1.0f)
+    {
+        yield return new WaitForSeconds(time);
+        gameObjectToPool.gameObject.SetActive(false);
+        pool.Enqueue(gameObjectToPool);
+    }
+
+    IEnumerator FlyTo(GameObject objectToMove, Transform end, float speed)
+    {
+        while (objectToMove.transform.position != end.position)
+        {
+            objectToMove.transform.position = Vector3.MoveTowards(
+                objectToMove.transform.position,
+                end.position,
+                speed * Time.deltaTime);
+
+            // Wait a frame and move again.
+            yield return null;
+        }
+    }
 }
